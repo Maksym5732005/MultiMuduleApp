c3feff82a7bc5f94437b30728c1034dd525484ed
m5732005@gmail.com <wdzCA5yU>
2021-05-04 14:47:07 +0300

    Update Splash UI

--- feature-splash/src/main/kotlin/com/examples/multimuduleapp/feature/splash/SplashFragment.kt
+++ feature-splash/src/main/kotlin/com/examples/multimuduleapp/feature/splash/SplashFragment.kt
@@ -1,10 +1,20 @@
 package com.examples.multimuduleapp.feature.splash
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.examples.multimuduleapp.feature.splash.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment(R.layout.fragment_splash) {
+
+    private lateinit var binding: FragmentSplashBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentSplashBinding.inflate(inflater)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
